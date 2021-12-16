02e9f164caea9d0a03ee3f822a41c9d3fb1c24e8
muthu578 <muthummk92@gmail.com>
2021-12-16 19:53:14 +0530

    css changes for profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -4,89 +4,17 @@ import { Card, Input, Row, Col } from "antd";
 
 import { PROFILE_IC } from "../../assets/images/icons/navigation/navigation";
 import "./Profile.scss";
-
-// export default function Profile() {
-//   return (
-//     <>
-{
-  /* <section className='profile'>
-  <div>
-    <div className='list-header'>
-      <div className='list-header__grp'>
-        <div className='flex-grp'>
-          <h1>
-            <PROFILE_IC />
-          </h1>
-          <p>
-            <span>Lucas Stark</span>
-            <label>Global Admin</label>
-          </p>
-        </div>
-      </div>
-    </div>
-    <div className='site-card-border-less-wrapper'>
-      <Card title='Profile' bordered={false} style={{ width: 300 }}>
-        <div>
-          <div>
-            <form
-              name='contactform'
-              className='contactform form-signin'
-              onSubmit={this.contactSubmit.bind(this)}>
-              <fieldset className='fieldset'>
-                <label>First Name</label>
-                <Input
-                  type='text'
-                  name='user_id'
-                  className='loginInput'
-                  placeholder='Enter First Name'
-                />
-              </fieldset>
-              <fieldset className='fieldset'>
-                <label>Last Name</label>
-                <Input
-                  type='password'
-                  className='loginInput'
-                  name='user_password'
-                  placeholder='Enter Last Name'
-                />
-              </fieldset>
-              <fieldset className='fieldset'>
-                <label>Email</label>
-                <Input
-                  type='password'
-                  className='loginInput'
-                  name='user_password'
-                  placeholder='Enter Email Address'
-                />
-              </fieldset>
-              <p className='local-user'>Local User</p>
-              <input
-                type='submit'
-                className='btn-primary btn-input'
-                value='Login'
-              />
-            </form>
-          </div>
-        </div>
-      </Card>
-    </div>
-    ,
-  </div>
-</section>; */
-}
-
-//     </>
-//   );
-// }
-
+import useForm from "../../helper/useForm";
+import validate from "../../helper/validationForm";
 const Profile = () => {
-  const [count, setCount] = useState(0);
+  const { values, errors, handleChange, handleSubmit } = useForm(
+    login,
+    validate
+  );
 
-  useEffect(() => setCount((currentCount) => currentCount + 1), []);
-
-  const handleIncrement = () => setCount((currentCount) => currentCount + 1);
-
-  const handleDecrement = () => setCount((currentCount) => currentCount - 1);
+  function login() {
+    console.log("No errors, submit callback called!");
+  }
 
   return (
     <section className='profile'>
@@ -105,49 +33,152 @@ const Profile = () => {
           </div>
         </div>
         <div className='site-card-border-less-wrapper'>
-          <Card title='Profile' bordered={false} style={{ width: 300 }}>
-            <div>
-              <div>
-                <form className='contactform form-signin'>
-                  <fieldset className='fieldset'>
-                    <label>First Name</label>
-                    <Input
-                      type='text'
-                      name='user_id'
-                      className='loginInput'
-                      placeholder='Enter First Name'
-                    />
-                  </fieldset>
-                  <fieldset className='fieldset'>
-                    <label>Last Name</label>
-                    <Input
-                      type='password'
-                      className='loginInput'
-                      name='user_password'
-                      placeholder='Enter Last Name'
-                    />
-                  </fieldset>
-                  <fieldset className='fieldset'>
-                    <label>Email</label>
-                    <Input
-                      type='password'
-                      className='loginInput'
-                      name='user_password'
-                      placeholder='Enter Email Address'
-                    />
-                  </fieldset>
-                  <p className='local-user'>Local User</p>
-                  <input
-                    type='submit'
-                    className='btn-primary btn-input'
-                    value='Login'
-                  />
-                </form>
-              </div>
-            </div>
-          </Card>
+          <Row gutter={24}>
+            <Col span={14}>
+              <Card title='Profile' bordered={false}>
+                <div>
+                  <div>
+                    <form onSubmit={handleSubmit} noValidate>
+                      <div className='field'>
+                        <label className='label'>FirstName</label>
+                        <div className='control'>
+                          <Input
+                            className={`loginInput input ${
+                              errors.fname && "is-danger"
+                            }`}
+                            type='text'
+                            placeholder='Enter First Name'
+                            name='fname'
+                            onChange={handleChange}
+                            value={values.fname || ""}
+                            required
+                          />
+                        </div>
+                        {errors.fname && (
+                          <p className='help is-danger error'>{errors.fname}</p>
+                        )}
+                      </div>
+                      <div className='field'>
+                        <label className='label'>Last Name</label>
+                        <div className='control'>
+                          <Input
+                            className={`loginInput input ${
+                              errors.lname && "is-danger"
+                            }`}
+                            type='text'
+                            name='lname'
+                            placeholder='Enter Last Name'
+                            onChange={handleChange}
+                            value={values.lname || ""}
+                            required
+                          />
+                        </div>
+                        {errors.lname && (
+                          <p className='help is-danger error'>{errors.lname}</p>
+                        )}
+                      </div>
+                      <div className='field'>
+                        <label className='label'>Email Address</label>
+                        <div className='control'>
+                          <Input
+                            autoComplete='off'
+                            className={`loginInput input ${
+                              errors.email && "is-danger"
+                            }`}
+                            type='email'
+                            name='email'
+                            placeholder='Enter Email Address'
+                            onChange={handleChange}
+                            value={values.email || ""}
+                            required
+                          />
+                          {errors.email && (
+                            <p className='help is-danger error'>
+                              {errors.email}
+                            </p>
+                          )}
+                        </div>
+                      </div>
+                      <div className='btn-grp'>
+                        <button
+                          type='submit'
+                          className='button is-block is-info is-fullwidth ant-btn-primary'>
+                          Save
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </Card>
+            </Col>
+            <Col span={10}>
+              <Card title='Account Information' bordered={false}>
+                <div className='accout_grp'>
+                  <div className='accout_grp--details'>
+                    <Row>
+                      <Col span={12}>
+                        {" "}
+                        <label>User Name</label>
+                      </Col>
+                      <Col span={12}>
+                        {" "}
+                        <span>AAAAAA</span>
+                      </Col>
+                    </Row>
+                  </div>
+                  <div className='accout_grp--details'>
+                    <Row>
+                      <Col span={12}>
+                        {" "}
+                        <label>API Role</label>
+                      </Col>
+                      <Col span={12}>
+                        {" "}
+                        <span>Global Admin</span>
+                      </Col>
+                    </Row>
+                  </div>
+                  <div className='accout_grp--details'>
+                    <Row>
+                      <Col span={12}>
+                        {" "}
+                        <label>Web Role</label>
+                      </Col>
+                      <Col span={12}>
+                        {" "}
+                        <span>Web Global Admin</span>
+                      </Col>
+                    </Row>
+                  </div>
+                  <div className='accout_grp--details'>
+                    <Row>
+                      <Col span={12}>
+                        {" "}
+                        <label>Created Date</label>
+                      </Col>
+                      <Col span={12}>
+                        {" "}
+                        <span>2019/05/10 04:50:28</span>
+                      </Col>
+                    </Row>
+                  </div>
+                  <div className='accout_grp--details no-margin'>
+                    <Row>
+                      <Col span={12}>
+                        {" "}
+                        <label>Updated Date</label>
+                      </Col>
+                      <Col span={12}>
+                        {" "}
+                        <span>2019/05/10 04:50:28</span>
+                      </Col>
+                    </Row>
+                  </div>
+                </div>
+              </Card>
+            </Col>
+          </Row>
         </div>
-        ,
       </div>
     </section>
   );
